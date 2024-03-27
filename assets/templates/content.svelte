c7bf860f069ebc011c020491f7b042140c08b0f8
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-03-27 15:34:10 +0200

    draft

--- assets/templates/content.svelte
+++ assets/templates/content.svelte
@@ -5,32 +5,71 @@
   import Footer    from "../../../components/general/Footer.svelte";
 
   // static.
-  import { page }   from '$app/stores';
-  import { router } from '../../../stores/router.js';
+  import { onMount } from "svelte";
+  import { page }    from '$app/stores';
+  import { router }  from '../../../stores/router.js';
 
-  // highlight.
-  import 'highlight.js/styles/github-dark-dimmed.min.css';
-  import goLang      from 'highlight.js/lib/languages/go';
-  import hljs        from 'highlight.js/lib/core';
-  import { onMount } from 'svelte';
-  onMount(() => {
-    document.querySelectorAll('.article pre code').forEach((block) => {
-      hljs.highlightBlock(block);
-    });
+  let component;
+  let styles;
+
+  // highlight
+  import {HighlightAuto, LineNumbers } from "svelte-highlight";
+  import atomOneDark from "svelte-highlight/styles/atom-one-dark";
+  onMount(async () => {
+      component = (await import("svelte-highlight")).HighlightAuto;
+      styles = (await import("svelte-highlight/styles/github")).default;
   });
-  hljs.registerLanguage('go', goLang);
+
+  // content.
   let htmlContent = `{{index . "main.md"}}`
+  let blocks = [];
+
+  onMount(() => {
+      parseContent();
+  });
+  function decodeHtml(html) {
+      var txt = document.createElement('textarea');
+      txt.innerHTML = html;
+      return txt.value;
+  }
+  function parseContent() {
+      const regex = /<pre><code[^>]*>([\s\S]*?)<\/code><\/pre>/g;
+      let lastIndex = 0;
+      blocks = [];
+
+      htmlContent.replace(regex, (match, codeContent, index) => {
+          if (index > lastIndex) {
+              blocks.push({ type: 'text', content: decodeHtml(htmlContent.slice(lastIndex, index)) });
+          }
+          blocks.push({ type: 'code', content: decodeHtml(codeContent) });
+          lastIndex = index + match.length;
+      });
+      if (lastIndex < htmlContent.length) {
+          blocks.push({ type: 'text', content: htmlContent.slice(lastIndex) });
+      }
+  }
 </script>
 
 <svelte:head>
     <title>{$router[$page.url.pathname].static.title || 'GoLang etc.'}</title>
+    {@html atomOneDark}
 </svelte:head>
 
 <Header/>
 <Content>
     <div class="article">
         <div class="block-content">
-            {@html htmlContent}
+            {#each blocks as block}
+                {#if block.type === 'code'}
+                    <svelte:component
+                            this={component}
+                            langtag
+                            code={block.content}
+                    />
+                {:else}
+                    {@html block.content}
+                {/if}
+            {/each}
         </div>
     </div>
 </Content>
